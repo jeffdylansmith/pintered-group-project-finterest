834e8fef4231767bf7171cfd453f5d673a03644d
ollieOsh <sosinusi@gmail.com>
2017-06-07 13:59:02 -0500

    that took longer than it should have

--- app/controllers/pinModalCtrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-//on pin click show modal of the pin. add click events to save pin to
-
-app.controller("PinModalCtrl", function () {
-	console.log("~ PinModalCtrl yay! ~");
-	// body...
-});
---
+++ app/controllers/userPinsCtrl.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//on pin click show modal of the pin. add click events to save pin to
+
+app.controller("UserPinsCtrl", function (DataFactory, $scope, $location, authFactory) {
+	console.log("~ UserPinsCtrl yay! ~");
+
+	$scope.getUserPins = function(){
+		let user = authFactory.getUser();
+		console.log("user", user);
+		DataFactory.getUserPins(user)
+		.then((pins) => {
+			console.log("pins", pins);
+			$scope.pins = pins;
+		});
+	};
+
+	$scope.getUserPins();
+});
