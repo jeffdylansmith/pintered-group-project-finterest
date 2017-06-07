cbcc5513fcb6f59ccbdc2e258e7a015c95958354
ollieOsh <sosinusi@gmail.com>
2017-06-07 11:28:23 -0500

    basic add pin functionality firing. still need to add to specific board

--- app/controllers/addPinCtrl.js
+++ app/controllers/addPinCtrl.js
@@ -2,8 +2,21 @@
 
 //add new pin to FB
 
-app.controller("AddPinCtrl", function () {
+app.controller("AddPinCtrl", function ($scope, DataFactory, authFactory) {
 	console.log("~ AddPinCtrl yay! ~");
 	// body...
 
+	let user = authFactory.getUser();
+
+	$scope.pin = {
+		uid: user,
+		boardid: "",
+		url: "",
+		title: "",
+
+	};
+	DataFactory.getUserBoards();
+	$scope.addPin = () => {
+		DataFactory.addPin($scope.pin);
+	};
 });
